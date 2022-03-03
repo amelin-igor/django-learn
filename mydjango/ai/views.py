e27478c57c64429d47c107ee56f087ae828fea6e
amelin-igor <finlab@mail.ru>
2022-03-03 20:12:45 +0300

    добавлен URLS my_condition_3

--- mydjango/ai/views.py
+++ mydjango/ai/views.py
@@ -235,7 +235,7 @@ def add(request):
     v1 = my_condition_2(request)
 
     if v1["identificator"] == "00001" and name == "Vestka":
-        if co2 > 70:
+        if int(co2) > 70:
             v1["dev_1"] = True
 
     return JsonResponse(v1, safe=False)
